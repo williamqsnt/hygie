b920c9d5d975979f31fa669d7b09fe7d0828230a
Maxime <98897255+Blamax23@users.noreply.github.com>
2024-10-09 12:17:09 +0200

    Gestion du quiz et affichage des résultats

--- Hygie.Front/Controllers/QuizController.cs
+++ Hygie.Front/Controllers/QuizController.cs
@@ -2,6 +2,7 @@
 using Hygie.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace Hygie.Front.Controllers
 {
@@ -22,6 +23,42 @@ namespace Hygie.Front.Controllers
             Quiz quiz = _quizService.GetQuiz(quizCat);
 
             return View(quiz);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostResultQuiz(QuizCat quizCat, string valuesAnswers)
+        {
+            Quiz quiz = _quizService.GetQuiz(quizCat);
+            List<string> recommandations = new List<string>();
+            int[][] resultats = JsonSerializer.Deserialize<int[][]>(valuesAnswers);
+
+            foreach (var answer in resultats)
+            {
+                if (answer[1] == 0)
+                {
+                    recommandations.Add(quiz.Questions[answer[0]].Reponses.FirstOrDefault(r => r.Score == answer[1]).Recommandation);
+                }
+            }
+
+            return Json(recommandations);
+        }
+
+        [Route("/result")]
+        public ActionResult GetResultQuiz(string valuesAnswers, QuizCat quizCat)
+        {
+            Quiz quiz = _quizService.GetQuiz(quizCat);
+            List<string> recommandations = new List<string>();
+            int[][] resultats = JsonSerializer.Deserialize<int[][]>(valuesAnswers);
+
+            foreach (var answer in resultats)
+            {
+                if (answer[1] == 0)
+                {
+                    recommandations.Add(quiz.Questions[answer[0]].Reponses.FirstOrDefault(r => r.Score == answer[1]).Recommandation);
+                }
+            }
+
+            return View(recommandations);
         }
     }
 }
